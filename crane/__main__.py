ee7b2fd750c83f620beade53d6e530be69fb1278
actualaardvark <114784410+actualaardvark@users.noreply.github.com>
2025-02-17 16:25:25 -0800

    decimation

--- crane/__main__.py
+++ crane/__main__.py
@@ -1,21 +1,30 @@
-import numpy as np
 import cv2
+import os
 
-SOURCE_IMAGE = cv2.imread("./input.png", cv2.IMREAD_GRAYSCALE)
 CAM = cv2.VideoCapture(0)
 
-frame_width = int(CAM.get(cv2.CAP_PROP_FRAME_WIDTH))
-frame_height = int(CAM.get(cv2.CAP_PROP_FRAME_HEIGHT))
 
+def makepathifnotexist(path: str):
+    if not os.path.exists(path):
+        os.makedirs(path)
+
+
+framecounter: int = 0
 while True:
-    # Capture frame-by-frame
     ret, frame = CAM.read()
-
-    # Our operations on the frame come here
+    frame_height, frame_width, _ = frame.shape
+    decimate_factor = 2
+    decimate_point: tuple[int, int] = (
+        int(frame_width / decimate_factor),
+        int(frame_height / decimate_factor),
+    )
+    print(decimate_point)
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
-    # Display the resulting frame
-    cv2.imwrite("./1.png", gray)
+    makepathifnotexist(f"./pipeline_debug/{framecounter}")
+    cv2.imwrite(f"./pipeline_debug/{framecounter}/grayscale.png", gray)
+    decimate = cv2.resize(gray, decimate_point, interpolation=cv2.INTER_LINEAR)
+    cv2.imwrite(f"./pipeline_debug/{framecounter}/decimate.png", decimate)
+    # framecounter += 1
 
 # When everything done, release the capture
 CAM.release()
